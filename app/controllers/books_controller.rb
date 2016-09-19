c80a37d19204de093f49242083326e246cf8aed2
Hein Dauven <heindauven@gmail.com>
2016-09-19 15:36:04 +0200

    Added the models and corresponding tests. Updated the database according to the model schematics. Added relationships between the models.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+
+
   def new
     @page_title = 'Add New Book'
     @author = Author.new
@@ -24,4 +26,11 @@ class BooksController < ApplicationController
 
   def show
   end
+
+  private 
+    def book_params
+      params.require(:book).permit(
+        :title, :author_id, :category_id, :publisher_id, 
+        :isbn, :price, :buy, :format, :excerpt, :pages, :year)
+    end 
 end
