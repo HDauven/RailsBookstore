7a9adf615d012dfa50f932315788d0dc0339667f
Hein Dauven <heindauven@gmail.com>
2016-09-20 15:46:32 +0200

    Expanded all models with corresponding CRUD views. Added code to the controllers for CRUD operations.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,18 +16,37 @@ class CategoriesController < ApplicationController
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    @category.update(category_params)
+
+    flash[:notice] = 'Category Updated'
+
+    redirect_to categories_path
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def destroy
+    @category = Category.find(params[:id])
+
+    @category.destroy
+
+    flash[:notice] = 'Category Removed'
+
+    redirect_to categories_path
   end
 
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @categories = Category.all
+    @books = @category.books
   end
 
   private 
